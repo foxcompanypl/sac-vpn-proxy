ef6968503c757a13a2b496b3753b6731a75735d0
Łukasz Lis <ll.lukasz.lis@gmail.com>
2023-07-24 22:10:42 +0200

    CI/CD (#3)
    * feat: release and publish workflows
    
    * chore: release 1.0.8
    
    * feat: update publish ci
    
    * chore: release 1.0.9
    
    * feat: update release ci
    
    * chore: release 1.0.10
    
    * feat: update release ci token to custom
    
    * chore: release 1.0.11
    
    * feat: update publish ci docker hub image name
    
    * chore: release 1.0.12
    
    * feat: update publish ci context
    
    * chore: release 1.0.13
    
    * feat: update publish ci meta
    
    * chore: release 1.0.14
    
    * feat: optimize dockerfile cleanup
    
    * chore: release 1.0.15

--- release.sh
+++ release.sh
@@ -3,21 +3,10 @@ REPO=$(git config --get remote.origin.url | grep -Po "(?<=git@github\.com:)(.*?)
 branch=$(git symbolic-ref --short HEAD)
 version=""
 latestVersion="latest"
-if [ "$branch" == "master" ]; then
-    version=$(docker run --rm alpine/semver semver -c -i $1 $(cat VERSION))
-    echo "Version: $version"
-    echo "$version" >VERSION
-    git commit -am "chore: release $version"
-    git tag -a "$version" -m "version $version"
-    git push origin $branch
-    git push --tags
-else
-    version=$(git log -1 --pretty=format:%h)
-    latestVersion="$branch"
-fi
-docker build \
-    --tag $REPO:$version \
-    --tag $REPO:$latestVersion \
-    --no-cache \
-    ./build
-docker push --all-tags $REPO
+version=$(docker run --rm alpine/semver semver -c -i $1 $(cat VERSION))
+echo "Version: $version"
+echo "$version" >VERSION
+git commit -am "chore: release $version"
+git tag -a "$version" -m "version $version"
+git push origin $branch
+git push --tags
